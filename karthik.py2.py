789902033ff11ba9d74ef80c3f1e515f34811d13
KarthikVarma0304 <varmakarthik0304@gmail.com>
2025-12-28 21:47:29 +0530

    Add files via upload

---
+++ karthik.py2.py
@@ -0,0 +1,10 @@
+secret = 456
+guess = int(input("enter the number between 1 to 1000"))
+while guess != secret:   
+   guess = int(input("your guess in wrong")) 
+   # the loop ends here if the guess is correct#
+
+if guess == secret:#i used this if statement to print the guess is correct 
+   print("congrats your guess is correct")
+
+    
